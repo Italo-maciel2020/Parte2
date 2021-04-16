20101f75d1e1c06231e4630baca488c505ce6cca
Italo_maciel2020 <39493130+Italo-maciel2020@users.noreply.github.com>
2021-04-16 11:06:34 -0300

    Add files via upload

---
+++ interpretador.h
@@ -0,0 +1,14 @@
+#ifndef INTERPRETADOR_H
+#define INTERPRETADOR_H
+#include "figurageometrica.h"
+
+class Interpretador:: public FiguraGeometrica{
+public:
+    Interpretador::Interpretador();
+    int getDimx();
+    int getDimy();
+    int getDimz();
+
+};
+
+#endif // INTERPRETADOR_H
